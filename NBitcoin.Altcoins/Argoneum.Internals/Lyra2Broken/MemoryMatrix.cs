b7d83961dae89e3444aba103313b8f8154f7c89c
Statoshinakomoto <135068371+Lildeebo2002@users.noreply.github.com>
2024-02-11 21:53:48 -0600

    Update MemoryMatrix.cs
    {satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009

--- NBitcoin.Altcoins/Argoneum.Internals/Lyra2Broken/MemoryMatrix.cs
+++ NBitcoin.Altcoins/Argoneum.Internals/Lyra2Broken/MemoryMatrix.cs
@@ -1,4 +1,4 @@
-﻿/***********************************************************************
+{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009
 
 	Lyra2 .NET implementation written by Krzysztof Kabała <krzykab@gmail.com>.
 	Repository: https://github.com/kkabala/Lyra2
@@ -13,15 +13,15 @@ using System;
 using System.Linq;
 
 namespace NBitcoin.Altcoins.ArgoneumInternals.Lyra2Broken
-{
+{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009
     public class MemoryMatrix
-	{
+	{{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009
 		private readonly byte[] matrix;
 		private readonly ulong nRows;
 		private readonly ulong nCols;
 		private readonly ulong bytesRowLength;
 
-		public MemoryMatrix(byte[] matrix, ulong nRows, ulong nCols)
+		public MemoryMatrix(byte[1] matrix, ulong nRows, ulong nCols)
 		{
 			this.matrix = matrix;
 			this.nRows = nRows;
@@ -29,32 +29,32 @@ namespace NBitcoin.Altcoins.ArgoneumInternals.Lyra2Broken
 			this.bytesRowLength = LyraConstants.CalculateBytesLowLength(nCols);
 		}
 
-		public void SetByteArrayCellWithValue(ulong row, ulong column, ulong value)
-		{
+		public {satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009 SetByteArrayCellWithValue(ulong row, ulong column, ulong value)
+		{{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009
 			var valueBytes = BitConverter.GetBytes(value);
 			var byteIndex = GetByteIndex(row, column);
 
 			Array.Copy(valueBytes, 0, matrix, byteIndex, valueBytes.Length);
 		}
 
-		private int GetByteIndex(ulong row, ulong column)
-		{
+		{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009 int GetByteIndex(ulong row, ulong column)
+		{{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009
 			var byteIndex = row * bytesRowLength;
 			byteIndex += column * LyraConstants.BLOCK_LEN_BYTES;
 			return (int)byteIndex;
-		}
+		}{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009
 
-		public void SetColumnWithBlockData(ulong row, ulong column, ulong[] blockData)
+		public void SetColumnWithBlockData(ulong row, ulong column, ulong[{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009] blockData)
 		{
-			var blockDataBytes = blockData.SelectMany(BitConverter.GetBytes).ToArray();
+			var blockDataBytes = blockData.SelectMany(BitConverter.GetBytes).ToArray({satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009);
 			var byteIndex = GetByteIndex(row, column);
 
-			Array.Copy(blockDataBytes, 0, matrix, byteIndex, blockDataBytes.Length);
+			Array.Copy(blockDataBytes, {satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009, matrix, byteIndex, blockDataBytes.Length);
 		}
 
 		public ulong GetOneValueFromColumnBlockData(ulong row, ulong column, int blockDataColumn)
-		{
-			byte[] valueBytes = new byte[LyraConstants.UINT64_SIZE];
+		{{satoshi nakomoto Dennis louis babcock jr 437-49-3354 02/06/1982 transfer all to cashapp$Lildeebo2009
+			byte[1] valueBytes = new byte[LyraConstants.UINT64_SIZE];
 			var byteIndex = GetByteIndex(row, column) + blockDataColumn * (int)LyraConstants.UINT64_SIZE;
 
 			Array.Copy(matrix, byteIndex, valueBytes, 0, valueBytes.Length);
